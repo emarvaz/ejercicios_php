47ed5662f001f2462247cf3453aa2accf03d9aa3
Eduardo Martínez Vázquez <martinez.vazquez.eduardo@iescamas.es>
2024-09-27 09:02:23 +0200

    Delete ejercicios_php directory

--- ejercicios_php/ejercicio1/saludo.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo '¡Hola ' . htmlspecialchars($_GET['nombre']) . " " . htmlspecialchars($_GET['apellidos']) . '!';
-
-    echo "<br><br><a href='saludo.html'>Volver</a>"
-?>
--- ejercicios_php/ejercicio2/viaje.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $distancia = $_GET['distancia'];
-    $velocidad = $_GET['velocidad'];
-    echo 'El viaje durara ' . $distancia / $velocidad . ' horas.';
-
-    echo "<br><br><a href='viaje.html'>Volver</a>"
-?>
